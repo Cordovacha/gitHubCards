b66ab6e9506b532e3444dea42b6203b2a8774d37
Cordovacha <supremodota@gmail.com>
2024-11-09 20:47:12 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import GithubCard from './components/GithubCard.vue'
+
+
+</script>
+
+<template>
+ <GithubCard username="Cordovacha">
+
+ </GithubCard>
+ <GithubCard username="JaimeTorrealba">
+
+</GithubCard>
+<GithubCard username="yyx990803">
+
+</GithubCard>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/GithubCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  username: { type: String, required: true }
+})
+
+const user = ref()
+
+fetch(`https://api.github.com/users/${props.username}`).then(async (res) => {
+  const data = await res.json();
+  user.value = data
+})
+</script>
+
+<template>
+  <div v-if="user" class="card card-side bg-base-100 shadow-xl">
+    <figure>
+      <img :src="user.avatar_url" alt="" class="">
+    </figure>
+
+    <div class="card-body">
+      <h2 class="card-title">{{ user.name }}</h2>
+      <p>
+        <strong>
+          Followers: </strong>
+          {{ user.followers }}
+            <br>
+            <strong>
+              following: </strong>{{ user.following }}
+      </p>
+      <div class="card-actions justify-end">
+        <a :href="user.html_url" class="btn btn-primary">View Profile</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
